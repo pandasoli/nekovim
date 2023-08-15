fe9024b6d8465bbe2b8446bd9d24410c5be33eb9
Panda Soli <pandasoli@vivaldi.net>
2023-08-15 18:18:41 -0300

    feat(types): make fields not required

--- lua/types/presence.lua
+++ lua/types/presence.lua
@@ -3,18 +3,18 @@
 ---@field url   string
 
 ---@class PresenceAssets
----@field large_image string
----@field large_text  string
----@field small_image string
----@field small_text  string
+---@field large_image? string
+---@field large_text?  string
+---@field small_image? string
+---@field small_text?  string
 
 ---@class PresenceTimestamps
----@field start integer
----@field end   integer
+---@field start? integer
+---@field end?   integer
 
 ---@class Presence
----@field state      string
----@field details    string
----@field timestamps PresenceTimestamps
----@field assets     PresenceAssets
----@field buttons    PresenceButton[]
+---@field state?      string
+---@field details?    string
+---@field timestamps? PresenceTimestamps
+---@field assets?     PresenceAssets
+---@field buttons?    PresenceButton[]
--- lua/types/presence_makers.lua
+++ lua/types/presence_makers.lua
@@ -1,17 +1,17 @@
 ---@class PresenceMakersAssets : PresenceAssets
----@field large_image fun(self: NekoVim)
----@field large_text  fun(self: NekoVim)
----@field small_image fun(self: NekoVim)
----@field small_text  fun(self: NekoVim)
+---@field large_image? (fun(self: NekoVim): string)|string
+---@field large_text?  (fun(self: NekoVim): string)|string
+---@field small_image? (fun(self: NekoVim): string)|string
+---@field small_text?  (fun(self: NekoVim): string)|string
 
 ---@class PresenceMakersTimestamps : PresenceTimestamps
----@field start fun(self: NekoVim)
----@field end   fun(self: NekoVim)
+---@field start? (fun(self: NekoVim): integer)|integer
+---@field end?   (fun(self: NekoVim): integer)|integer
 
 ---@class PresenceMakers : Presence
----@field app_id  string
+---@field app_id  (fun(self: NekoVim): string)|string
 ---
----@field state   fun(self: NekoVim)
----@field details fun(self: NekoVim)
----@field assets  PresenceMakersAssets
----@field buttons fun(self: NekoVim)[]
+---@field state?   (fun(self: NekoVim): string)|string
+---@field details? (fun(self: NekoVim): string)|string
+---@field assets?  PresenceMakersAssets
+---@field buttons? ((fun(self: NekoVim): PresenceButton)|PresenceButton)[]
